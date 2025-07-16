bc673d7eaf52518008f98c8ed58d32b5f7ff0848
Devesh Ghodpage <deveshghodpage333@yahoo.in>
2025-07-16 19:41:36 +0530

    Intro to C#

---
+++ Assets/Scripts/Intro_to_CSharp.cs
@@ -0,0 +1,30 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Intro_to_CSharp : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+        
+//     }
+// }
+
+
+using System;
+
+namespace HelloWorld{
+    class Program{
+        public static void Main(string[] args){
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
